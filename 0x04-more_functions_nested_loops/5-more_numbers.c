10628e207ec10c2c87fe1ff53b5846ed460aa6d1
ArtisanGray <1697@holbertonschool.com>
2020-09-24 19:13:33 +0000

    fix for checks on 5 and 7

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,16 @@
 */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10; j++)
 	{
-		if (i >= 10)
-			_putchar('0' + i / 10);
-		_putchar('0' + i % 10);
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,18 @@
 void print_diagonal(int n)
 {
 	int i, j;
+
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-				for (j = 0; j <= i; j++)
-				{
-					if (j == i)
-						_putchar('\\');
-					else
-						_putchar(' ');
-				}
+			for (j = 0; j <= i; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
