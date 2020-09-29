11e605040e161a5bd9b6059853f25a71dd7830ce
ArtisanGray <1697@holbertonschool.com>
2020-09-29 21:52:50 +0000

    added 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _atoi - converts string to integer
+* @s: string pointer
+*
+* Return: always 0
+*/
+int _atoi(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+	return (0);
+}
