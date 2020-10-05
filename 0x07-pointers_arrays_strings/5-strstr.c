9dbcdf3afd5a788820dba1b8fae0dbb452798431
ArtisanGray <1697@holbertonschool.com>
2020-10-05 19:43:26 +0000

    possible fix for 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
 * _strstr - finds a substring within a string
-* @haystack - string to search through
-* @needle - string to search for
+* @haystack: string to search through
+* @needle: string to search for
 *
 * Return: null on fail, location of substring on success
 */
@@ -10,20 +10,23 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, mat = 0;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	if (haystack != NULL && needle != NULL)
 	{
-		if (haystack[i] == needle[mat])
+		for (i = 0; haystack[i] != '\0'; i++)
 		{
-			mat++;
-			if (needle[mat + 1] == '\0')
+			if (haystack[i] == needle[mat])
 			{
-				return (haystack + (i - (mat - 1)));
+				mat++;
+				if (needle[mat + 1] == '\0')
+				{
+					return (haystack + (i - (mat - 1)));
+				}
 			}
-		}
-		else if (haystack[i] != needle[mat] && mat > 0)
-		{
-			i -= mat - 1;
-			mat = 0;
+			else if (haystack[i] != needle[mat] && mat > 0)
+			{
+				i -= mat - 1;
+				mat = 0;
+			}
 		}
 	}
 	return (NULL);
