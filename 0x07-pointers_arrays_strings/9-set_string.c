7b38a45e745bd003a9c860df474b38f5a3551934
ArtisanGray <1697@holbertonschool.com>
2020-10-05 20:01:19 +0000

    created a guess for 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* set_string - sets a string pointer
+* @s: string pointer pointer
+* @to: string to set string to
+*
+* Return: null
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
