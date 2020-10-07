aa623f0653eb9fbc7a153492d1e441fcf05bf948
ArtisanGray <1697@holbertonschool.com>
2020-10-07 19:42:13 +0000

    betty fix on 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
+* _print_rev_recursion - prints a string in reverse using recursion
+* @s: string pointer
 *
 *
-*
-*
-*
+* Return: null
 */
 void _print_rev_recursion(char *s)
 {
