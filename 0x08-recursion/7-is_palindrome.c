1c3da15189639002d93a9c06b6f76c3289e98db0
ArtisanGray <1697@holbertonschool.com>
2020-10-07 22:29:10 +0000

    betty fix for 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -14,9 +14,12 @@ int _strlen_recursion(char *s)
 	return (0);
 }
 /**
+* palindrome - checks if string is a palindrome
+* @i: starting point of string
+* @len: ending point of string
+* @s: string
 *
-*
-*
+* Return: 0 if not, 1 if success
 */
 int palindrome(int i, int len, char *s)
 {
@@ -29,17 +32,18 @@ int palindrome(int i, int len, char *s)
 	return (1);
 }
 /**
+* is_palindrome - checks if a word is palindrome
+* @s: string pointer
 *
-*
-*
-*
+* Return: 0 on fail, 1 on success
 */
 int is_palindrome(char *s)
 {
 	int fr = 0;
 	int bk = _strlen_recursion(s) - 1;
 	int is_palin = palindrome(fr, bk, s);
+
 	if (*s == '\0')
 		return (1);
-	return(is_palin);
+	return (is_palin);
 }
