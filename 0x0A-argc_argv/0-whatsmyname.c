4098fa0ae3754c1f0d87b65f1165b38a58c60d5a
Eli Greene <1697@holbertonschool.com>
2020-10-11 22:12:00 +0000

    fixed 0?

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <limits.h>
-
 /**
 * main - prints the name of the executed program
 * @argc: count of arguments
@@ -12,11 +9,6 @@
 */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc != 0)
-	{
-		printf("%s\n", argv[0]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
