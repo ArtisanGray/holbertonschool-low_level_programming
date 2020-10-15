a454500513578ac911c2af0c8ebfe7ef0d74404e
ArtisanGray <1697@holbertonschool.com>
2020-10-15 20:20:22 +0000

    inital test commit for 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* malloc_checked - allocates memory with b bytes
+* @b: bytes to allocate
+*
+*
+* Return: new allocated void pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit (98);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+A collection of C files that utilize memory allocation and freeing
