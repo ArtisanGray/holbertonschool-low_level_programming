4b4b5a159e8835c66b047e12e6dfc13a9c54fd02
ArtisanGray <1697@holbertonschool.com>
2020-10-19 21:46:54 +0000

    reverted to previous commit, maybe null byte check?

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,7 +25,22 @@ int _strlen(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *stanley;
-	char *stanname = name, *stanowner = owner;
+	char *stanname, *stanowner;
+	int i, j;
+
+	stanname = malloc(sizeof(char) * _strlen(name) + 1);
+	/*allocates space for a copy of the dog's name*/
+	if (stanname == NULL)
+		return (NULL);
+	stanowner = malloc(sizeof(char) * _strlen(owner) + 1);
+	/*allocates space for a copy of the owner's name*/
+	if (stanowner == NULL)
+		return (NULL);
+
+	for (i = 0; i < _strlen(name) + 1; i++)
+		stanname[i] = name[i];
+	for (j = 0; j < _strlen(owner) + 1; j++)
+		stanowner[j] = owner[j];
 
 	stanley = malloc(sizeof(dog_t));
 	if (stanley == NULL)
