d4e4e55cf768388d8dbd753344c011249fe0a4ec
ArtisanGray <1697@holbertonschool.com>
2020-10-25 03:51:29 +0000

    possible test case fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 
 	va_start(vl, format);
 
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		fail = 0;
 		switch (format[i])
